75fe4f56ed20d09ba1c300de63c114fd8542ae3b
Aniela Lapinski <Aniela.Lapinski@magnetforensics.com>
2023-05-06 21:30:50 -0400

    removing commenting

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,20 +1,14 @@
 
 import React from 'react'
-// import MenuBar from '../components/navigations/MenuBar'
-// import Footer from '../components/navigations/Footer'
 
 import '../styles/Layout.css'
 
 const Layout = (props) => {
   return(
     <React.Fragment>
-      {/* <MenuBar /> */}
-
       <main className="main-content">
         {props.children}
       </main>
-
-      {/* <Footer/> */}
     </React.Fragment>
   );
 }
